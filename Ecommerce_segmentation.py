e717707821c038e489cfd9a21c882d311d1d0b6a
Tejal Mehta <tejal.mehta@city.ac.uk>
2023-04-18 10:04:26 -0700

    plot updates with plotly

--- Ecommerce_segmentation.py
+++ Ecommerce_segmentation.py
@@ -7,11 +7,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import warnings
-import plotly.express as px
 warnings.filterwarnings('ignore')
 # sns.set_style('whitegrid')
+import mpld3
 from operator import attrgetter
 import datetime
+import plotly.graph_objects as go
+import plotly.express as px
 
 # Set the title of the app
 st.title("E-Commerce Customer Data Segmentation")
@@ -98,42 +100,12 @@ with st.expander("Show the RFM Analysis "):
                r'[4-5][2-3]': 'Potential Loyalists',#recent customers and moderate buyers
                r'5[4-5]': 'Top Customers'}#recent customers and high frequecy
 
-
-    segments = rfm["segment"].value_counts().sort_values(ascending=False)
-#     fig = plt.gcf()
-#     ax = fig.add_subplot()
-#     fig.set_size_inches(25, 10)
-#     squarify.plot(
-#         sizes=segments,
-#         label=[label for label in seg_map.values()],
-#         color=[
-#             "#AFB6B5",
-#             "#F0819A",
-#             "#926717",
-#             "#F0F081",
-#             "#81D5F0",
-#             "#C78BE5",
-#             "#748E80",
-#             "#FAAF3A",
-#             "#7B8FE4",
-#             "#86E8C0",
-#         ],
-#         pad=False,
-#         bar_kwargs={"alpha": 1},
-#         text_kwargs={"fontsize": 15},
-#     )
-#     plt.title("Customer Segmentation Map", fontsize=20)
-#     plt.xlabel("Frequency", fontsize=10)
-#     plt.ylabel("Recency", fontsize=10)
-#     st.pyplot(plt.gcf())
-
     segments = rfm["segment"].value_counts().sort_values(ascending=False)
     
     fig = px.treemap(
     names=[label for label in seg_map.values()],
     parents=['' for _ in seg_map.values()],
     values=segments.values,
-    hovertemplate='<b>%{label}</b><br>Count: %{value}',
     color_discrete_sequence=px.colors.qualitative.Alphabet
     )
     fig.update_layout(
@@ -221,58 +193,55 @@ with st.expander("Show the Cohort Analysis "):
          - The number of customers in each cohort
          - The retention rate of each cohort over time
          - The overall retention rate of the customer base """)
+    data = df.copy()
+    #dropping duplicated
+    data = data[["CustomerID", "InvoiceNo", "InvoiceDate"]].drop_duplicates()
+    
+    #getting order month from date - to group transactions by month
+    data["order_month"] = data["InvoiceDate"].dt.to_period("M")
+    
+    #grouping customers and finding first purchase date for each customers
+    data["cohort"] = (data.groupby("CustomerID")["InvoiceDate"].transform("min").dt.to_period("M"))
+    
+    #aggregating the number of unique customers in each cohort for each period
+    cohort_data = (data.groupby(["cohort", "order_month"]).agg(n_customers=("CustomerID", "nunique")).reset_index(drop=False))
+    
+    # calculate the number of periods that have passed since a customer's first purchase
+    cohort_data["period_number"] = (cohort_data.order_month - cohort_data.cohort).apply(attrgetter("n"))
+   
+   #the retention rate of each cohort over time, which is helpful in identifying trends and patterns in customer behavior
+    cohort_pivot = cohort_data.pivot_table(index="cohort", columns="period_number", values="n_customers")
+    cohort_pivot.index = cohort_pivot.index.astype(str)
+    
+    #This line of code extracts the first column of the cohort_pivot pivot table, which represents the number of customers in the initial cohort. 
+    cohort_size = cohort_pivot.iloc[:, 0]
+    
+    retention_matrix = cohort_pivot.divide(cohort_size, axis=0)
+    # create a list of x and y tick labels
+    x_labels = retention_matrix.columns.tolist()
+    y_labels = retention_matrix.index.tolist()
 
-    def CohortAnalysis(dataframe):
+    # create the heatmap trace
+    heatmap = go.Heatmap(z=retention_matrix.round(3) * 100,
+                         x=x_labels,
+                         y=y_labels,
+                         colorscale="BuPu_r",
+                         zmin=0.0,
+                         zmax=50.0,
+                         colorbar=dict(title="%"),
+                         hovertemplate="Retention: %{z:.0f}%<br>" +
+                                   "Cohort Group : %{y}<br>" +
+                                   "Retention Period : %{x}" +
+                                   " months <extra></extra>")
 
-        data = dataframe.copy()
-        #dropping duplicated
-        data = data[["CustomerID", "InvoiceNo", "InvoiceDate"]].drop_duplicates()
-        
-        #getting order month from date - to group transactions by month
-        data["order_month"] = data["InvoiceDate"].dt.to_period("M")
-        
-        #grouping customers and finding first purchase date for each customers
-        data["cohort"] = (data.groupby("CustomerID")["InvoiceDate"].transform("min").dt.to_period("M"))
-        
-        #aggregating the number of unique customers in each cohort for each period
-        cohort_data = (data.groupby(["cohort", "order_month"]).agg(n_customers=("CustomerID", "nunique")).reset_index(drop=False))
-        
-        # calculate the number of periods that have passed since a customer's first purchase
-        cohort_data["period_number"] = (cohort_data.order_month - cohort_data.cohort).apply(attrgetter("n"))
-       
-       #the retention rate of each cohort over time, which is helpful in identifying trends and patterns in customer behavior
-        cohort_pivot = cohort_data.pivot_table(index="cohort", columns="period_number", values="n_customers")
-        
-        #This line of code extracts the first column of the cohort_pivot pivot table, which represents the number of customers in the initial cohort. 
-        cohort_size = cohort_pivot.iloc[:, 0]
-        
-        retention_matrix = cohort_pivot.divide(cohort_size, axis=0)
-        with sns.axes_style("white"):
-            fig, ax = plt.subplots(1, 2, figsize=(12, 8), sharey=True, gridspec_kw={"width_ratios": [1, 11]})
-            sns.heatmap(
-                retention_matrix,
-                mask=retention_matrix.isnull(),
-                annot=True,
-                cbar=False,
-                fmt=".0%",
-                cmap="coolwarm",
-                ax=ax[1],
-            )
-            ax[1].set_title("Monthly Cohorts: User Retention", fontsize=14)
-            ax[1].set(xlabel="Time since the first purchase", ylabel="Cohort")
-            white_cmap = mcolors.ListedColormap(["white"])
-            sns.heatmap(
-                pd.DataFrame(cohort_size).rename(columns={0: "cohort_size"}),
-                annot=True,
-                cbar=False,
-                fmt="g",
-                cmap=white_cmap,
-                ax=ax[0],
-            )
-            fig.tight_layout()
-            st.pyplot(plt.gcf())
-        
-    CohortAnalysis(df)
+    # create the figure layout
+    layout = go.Layout(title=dict(text="Monthly Cohorts : User Retention", x=0.5, y=0.9, xanchor='center', yanchor='top'),
+                       xaxis=dict(title="Cohort Period : Time since the first purchase"),
+                       yaxis=dict(title="Cohort Group : First Purchase"))
+
+    # create the figure and show it
+    fig = go.Figure(data=[heatmap], layout=layout)
+    st.plotly_chart(fig)
 
     st.write( """When a heatmap is used to display the retention rates of different cohorts over time, it becomes apparent that 50% of the customers who registered in December 2010 remained active after 11 months. Additionally, the retention rate of early sign-ups is higher than that of later ones.
      """)
